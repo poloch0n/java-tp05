a054812cd7f7b5688aae7f2271ba1d3dcae78cf3
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-06 11:49:51 +0200

    create pizzamemDao, ipizzadao, pizzamodel

---
+++ src/fr/pizzeria/IPizzaDao.java
@@ -0,0 +1,11 @@
+package fr.pizzeria;
+import fr.pizzeria.model.Pizza;
+
+public interface IPizzaDao {
+	Pizza[] findAllPizzas();
+	void saveNewPizza(Pizza pizza);
+	void updatePizza(String codePizza, Pizza pizza);
+	void deletePizza(String codePizza);
+	Pizza findPizzaByCode(String codePizza);
+	boolean pizzaExists(String codePizza);
+}
---
+++ src/fr/pizzeria/PizzaMemDao.java
@@ -0,0 +1,5 @@
+package fr.pizzeria;
+
+public class PizzaMemDao {
+
+}
